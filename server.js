d4f221dd7cf62635d0585d9de1b44ee400bb654d
kikegamba <we.gamba125@uniandes.edu.co>
2018-09-20 16:15:53 -0500

    Modificación del server.js cliente to client

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/dist/cliente6'));
+app.use(express.static(__dirname +'/dist/client6'));
 
 app.get('*', function (req, res) {
-  const index = path.join(__dirname, 'dist','cliente6', 'index.html');
+  const index = path.join(__dirname, 'dist','client6', 'index.html');
   res.sendFile(index);
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 5);
